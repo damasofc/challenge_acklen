c9fbd74666214db107d5c8d6908086d009c8f806
Damaso Fernandez <damasofc@hotmail.com>
2019-11-18 11:12:15 -0600

    starting challenge

---
+++ test/test.js
@@ -0,0 +1,30 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const {suite} = require('selenium-webdriver/testing');
+
+var chai = require('chai');
+var should = chai.should();
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var HomePage = require('../lib/HomePage');
+
+
+describe('Parking Cost Calculator Tests', () => {
+        let page;
+        before( async () => {
+          page = await new HomePage();
+          await page.visit('http://www.shino.de/parkcalc/');
+          
+        });
+      
+        after( async () => {
+          await page.quit();
+        });
+      
+        it('Has Text', async () => {
+            var res = await page.hasTextElements();
+            res.should.be.a('number');
+  
+        });
+      
+  })
+
